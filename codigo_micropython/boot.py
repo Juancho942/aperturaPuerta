586a80f984e4a641a492ccaa084436a259b3e59d
Juan David <juandavidbetancurvalencia>
2022-07-29 00:36:09 -0500

    ajuste programa y carcasa 29-07

--- codigo_micropython/boot.py
+++ codigo_micropython/boot.py
@@ -11,6 +11,7 @@ import time
 import network
 import _thread
 
+
 I2C_ADDR     = 0x27
 I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
@@ -23,22 +24,21 @@ luzPuertaAbierta = Pin(26, Pin.OUT)
 luzPuertaCerrada = Pin(25, Pin.OUT)
 aperturarf = Pin(27, Pin.IN)
 tiempoEncendido = 0
+tiempoExtraerDatos = 0
 acceso = False
 spi = 0
 lcd = 0
+conexion = False
+puntosConexion = 9
+puntos = 0
 
+    
 def int_ext(aperturarf):
     global acceso
     acceso = True
-
-def tiempo():
-    #global tiempoEncendido
-    tiempoEncendido = 1 + tiempoEncendido
     
 tim1 = Timer(1)
-tim1.init(period=2000, mode=Timer.PERIODIC, callback=lambda t:
-            tiempo()
-            )
+tim1.init(period=2000, mode=Timer.PERIODIC, callback=lambda t:tiempo())
 
 def escribir():
     global uid_card
@@ -54,13 +54,12 @@ def extraerDatos():
     firebase.setURL("https://ccirod-default-rtdb.firebaseio.com/")
     firebase.get("CCI/RFID", "var1", bg=0)
     uid_card = firebase.var1
-    print(uid_card)
-    print(type(uid_card))
             
             
 def Inserta_Nueva_Targeta():
     global spi
     global lcd
+    global uid_card
     luzPuertaCerrada.value(0)
     Sonido_ingreso_targeta()
     luzPuertaAbierta.value(1)
@@ -82,18 +81,35 @@ def Inserta_Nueva_Targeta():
             (stat, raw_uid) = rdr.anticoll()
             if stat == rdr.OK:
                 card_id = "0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
-                print(uid_card[:])
-                uid_card.append(str(card_id))
-                print(uid_card[:])
-                escribir()
-                ingresoTargeta = 0
-                lcd.clear()
-                lcd.move_to(1,0)
-                lcd.putstr("nueva targeta")
-                lcd.move_to(3,1)
-                lcd.putstr("ingresada")
-                cambioLcd = False
-                Sonido_ingreso_targeta()
+                if str(card_id) in uid_card:
+                    Sonido_Puerta_Denegada()
+                    ingresoTargeta = 0
+                    lcd.clear()
+                    lcd.move_to(0,0)
+                    lcd.putstr("Denegado targeta")
+                    lcd.move_to(3,1)
+                    lcd.putstr("ingresada")
+                    cambioLcd = False
+                elif str(card_id) in targetaMaestra:
+                    Sonido_Puerta_Denegada()
+                    ingresoTargeta = 0
+                    lcd.clear()
+                    lcd.move_to(0,0)
+                    lcd.putstr("Denegado targeta")
+                    lcd.move_to(4,1)
+                    lcd.putstr("Maestra")
+                    cambioLcd = False
+                else:
+                    Sonido_ingreso_targeta()
+                    uid_card.append(str(card_id))
+                    escribir()
+                    ingresoTargeta = 0
+                    lcd.clear()
+                    lcd.move_to(1,0)
+                    lcd.putstr("nueva targeta")
+                    lcd.move_to(3,1)
+                    lcd.putstr("ingresada")
+                    cambioLcd = False
 
 def Sonido_ingreso_targeta():
     sonido.value(1)
@@ -162,51 +178,86 @@ def tiempo():
     global tiempoEncendido
     tiempoEncendido += 1
 
+def mensajeConectando():
+    global puntosConexion
+    global puntos
+    if (puntosConexion > 16):
+        puntosConexion = 9
+        for i in range(16, 9, -1):
+            lcd.move_to(i,0)
+            lcd.putstr(" ")
+    lcd.move_to(puntosConexion,0)
+    lcd.putstr(".")
+    puntos += 1
+    if puntos > 10 :
+        puntosConexion += 1
+        puntos = 0
+def conectar():
+    global conexion
+    GLOB_WLAN=network.WLAN(network.STA_IF)
+    GLOB_WLAN.active(True)
+    try:
+        GLOB_WLAN.connect("Francelly", "25101291")
+    
+    except OSError:
+        lcd.clear()
+        lcd.move_to(0,1)
+        lcd.putstr("no se pudo")
+        lcd.move_to(1,1)
+        lcd.putstr("establecer conex")
+        print("no se establecio conexion")
+        conexion = True
+        pass
+    while not GLOB_WLAN.isconnected():
+        global conexion
+        mensajeConectando()
+        conexion = False
+        pass
+    
 def main():
     
     global spi
     global lcd
+    global uid_card
+    global extraerDatos
     
     i2c = I2C(0, sda=Pin(21), scl=Pin(22), freq=100000)
     lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
     lcd.clear()
     lcd.move_to(0,0)
-    lcd.putstr("connected....")
-    GLOB_WLAN=network.WLAN(network.STA_IF)
-    GLOB_WLAN.active(True)
-    GLOB_WLAN.connect("Francelly", "25101291")
-    while not GLOB_WLAN.isconnected():
-        pass
-
-    lcd.move_to(2,0)
-    lcd.putstr("conect wifi")
-    lcd.move_to(0,1)
-    lcd.putstr("obteniendo datos")
-    extraerDatos()
-    print(uid_card)
-    longitudDiccionario = len(uid_card)
-    print(longitudDiccionario)
+    lcd.putstr("connected")
+    conectar()
+    if conexion is False:
+        extraerDatos()
     spi = SPI(2, baudrate=2500000, polarity=0, phase=0)
     pantallaLcd = True
     aperturarf.irq(trigger=Pin.IRQ_RISING, handler=int_ext)
 
     while True:
+        
+        if conexion:
+            conectar()
+            
         global tiempoEncendido
         global acceso
+        global uid_card
+        
+        if tiempoEncendido > 20:
+            extraerDatos()
+            print("extraer datos")
+            tiempoEncendido = 0
         
         if acceso is True:
             lcd.backlight_on()
             lcd.clear()
             lcd.move_to(4,0)
-            lcd.putstr("ingreso")
-            lcd.move_to(2,1)
-            lcd.putstr("targeta RF")
+            lcd.putstr("Ingreso")
+            lcd.move_to(4,1)
+            lcd.putstr("Mando RF")
             pantallaLcd = False
-            print("abrir puerta")
             Abrir_Puerta()
             pantallaLcd = True
             tiempoEncendido = 0
-        print(tiempoEncendido)
         if (pantallaLcd):
             if(tiempoEncendido >2):
                 lcd.clear()
@@ -223,26 +274,30 @@ def main():
         luzPuertaAbierta.value(0)
         spi.init()
         rdr = MFRC522(spi=spi, gpioRst=4, gpioCs=5)
-        print("Place card")
         (stat, tag_type) = rdr.request(rdr.REQIDL)
         if stat == rdr.OK:
             (stat, raw_uid) = rdr.anticoll()
             if stat == rdr.OK:
                 card_id = "0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
-                print(card_id)
                         
-                if str(card_id) in str(uid_card):
+                if str(card_id) in targetaMaestra:
+                    lcd.backlight_on()
+                    Inserta_Nueva_Targeta()
+                    pantallaLcd = True
+                    tiempoEncendido = 0
+                    
+                elif str(card_id) in str(uid_card):
                     lcd.backlight_on()
                     lcd.clear()
                     lcd.move_to(4,0)
-                    lcd.putstr("ingreso")
+                    lcd.putstr("Ingreso")
                     lcd.move_to(2,1)
-                    lcd.putstr("targeta RF")
+                    lcd.putstr("Targeta RF")
                     pantallaLcd = False
-                    print("abrir puerta")
                     Abrir_Puerta()
                     pantallaLcd = True
                     tiempoEncendido = 0
+                    acceso = False
                     
                 else:
                     lcd.backlight_on()
@@ -254,9 +309,5 @@ def main():
                     pantallaLcd = True
                     Sonido_Puerta_Denegada()
                     tiempoEncendido = 0
-                if str(card_id) in targetaMaestra:
-                    print("inserta nueva targeta")
-                    Inserta_Nueva_Targeta()
-                    pantallaLcd = True
-                    tiempoEncendido = 0
+                
 main()
